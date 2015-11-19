26e2c902a9dae846eac60d4a25e4892ead2d699d
Fernando Iquiza <fmiquiza@gmail.com>
2015-11-19 05:33:57 -0400

    Personas
    ..

---
+++ public/scripts/controllers/personasCtrl.js
@@ -0,0 +1,10 @@
+'use strict'
+angular.module('app')
+	.controller('personasCtrl', function($scope,$http){
+		console.log("personasCtrl");
+    
+
+	})
+	;
+
+//creacion del 3er paso, nuestro controlador que sera referenciado como archivo dentro del index /public
