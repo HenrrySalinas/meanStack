0a9f29c82a676afa4fcfe5bd952712341c0b11c0
Fernando Iquiza <fmiquiza@gmail.com>
2015-11-19 07:03:17 -0400

    agregando dependencias..

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ var app=express();
 var mongojs=require('mongojs');
 var db=mongojs('meandb',['eventos','multimedia','LisNoticia']);
 var bodyParser=require('body-parser');
+
+var logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    expressSession = require('express-session'),
+    mongoose = require('mongoose'),
+    hash = require('bcrypt-nodejs'),
+    path = require('path'),
+    passport = require('passport'),
+    localStrategy = require('passport-local' ).Strategy;
+
 /***************************begin libs para carga de archivos*********************/
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart();
@@ -11,6 +22,65 @@ var upload = multer({ dest: 'uploads/' })
 eventsController = require('./controllers/eventosControllerServer');
 /***************************end libs para carga de archivos*********************/
 
+/*******************************************************************************/
+/*************** FHer Login command lines *************************************/
+// user schema/model
+var User = require('./server/models/user.js');
+
+// create instance of express
+var app = express();
+
+// require routes
+var routes = require('./server/routes/api.js');
+
+// define middleware
+app.use(express.static(path.join(__dirname, '../client')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// configure passport
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// routes
+app.use('/user/', routes);
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client', 'index.html'));
+});
+
+
+// error hndlers
+app.use(function(req, res, next) {
+    var err = new Error('No fue encontrado!!');
+    err.status = 404;
+    next(err);
+});
+
+app.use(function(err, req, res) {
+  res.status(err.status || 500);
+  res.end(JSON.stringify({
+    message: err.message,
+    error: {}
+  }));
+});
+
+module.exports = app;
+
+/******-------************-----Fin Fher command lines---------********----------/
+/*******************************************************************************/
 app.use(express.static(__dirname+'/public'));
 app.use("/uploads", express.static(__dirname + '/uploads'));
 app.use(bodyParser.json());
